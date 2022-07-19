2dad819b3cb3dc8be7883736053afc5bbdd39d3f
imgaram <s21049@gsm.hs.kr>
2022-07-19 23:16:02 +0900

    navigation drawer 구현

--- app/src/main/java/com/example/planer/MainActivity.kt
+++ app/src/main/java/com/example/planer/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.planer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.GravityCompat
+import com.example.planer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.imageDrawerOpenBtn.setOnClickListener {
+            binding.drawerLayout.openDrawer(GravityCompat.START)
+        }
     }
 }
