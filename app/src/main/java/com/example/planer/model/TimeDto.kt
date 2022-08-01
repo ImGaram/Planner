9961e3751ea57f52ab232192a878500964f16e95
imgaram <s21049@gsm.hs.kr>
2022-08-01 22:21:01 +0900

    일정을 담을 data class 생성

---
+++ app/src/main/java/com/example/planer/model/TimeDto.kt
@@ -0,0 +1,9 @@
+package com.example.planer.model
+
+data class TimeDto(
+    val id: Int,
+    val startTime: String,
+    val endTime: String,
+    val description: String,
+    val createUid: String
+)
