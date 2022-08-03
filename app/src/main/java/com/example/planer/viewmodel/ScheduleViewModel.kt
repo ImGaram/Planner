6fba479ab94a126b688529102cc22c919c211927
imgaram <s21049@gsm.hs.kr>
2022-08-03 22:13:17 +0900

    spinner string 배열 생성, 일정 data class 수정

---
+++ app/src/main/java/com/example/planer/viewmodel/ScheduleViewModel.kt
@@ -0,0 +1,44 @@
+package com.example.planer.viewmodel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.planer.model.ScheduleDto
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+
+class ScheduleViewModel: ViewModel() {
+    private val auth = FirebaseAuth.getInstance()
+    private val database = FirebaseDatabase.getInstance()
+    var result = MutableLiveData<Boolean>()
+    var scheduleDto = ScheduleDto()
+
+    fun createScheduleLogic(startTime: String, endTime: String, description: String) {
+        val scheduleList = arrayListOf<ScheduleDto>()
+
+        database.getReference("schedules").addListenerForSingleValueEvent(object :ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                for (data in snapshot.children) {
+                    val item = data.getValue(ScheduleDto::class.java)
+                    if (item != null) {
+                        scheduleList.add(item)
+                    }
+                }
+
+                scheduleDto.id = scheduleList.size + 1
+                scheduleDto.createUid = auth.currentUser?.uid
+                scheduleDto.startTime = startTime
+                scheduleDto.endTime = endTime
+                scheduleDto.description = description
+
+                database.getReference("schedules").child(scheduleDto.id.toString()).setValue(scheduleDto).addOnCompleteListener { task ->
+                    result.value = task.isSuccessful
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {}
+        })
+    }
+}
