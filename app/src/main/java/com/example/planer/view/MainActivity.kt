30c522434699acc647834245f575c7eeb7596941
imgaram <s21049@gsm.hs.kr>
2022-08-03 22:14:20 +0900

    일정 생성 기능 구현

--- app/src/main/java/com/example/planer/view/MainActivity.kt
+++ app/src/main/java/com/example/planer/view/MainActivity.kt
@@ -1,32 +1,43 @@
 package com.example.planer.view
 
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
 import android.widget.AdapterView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.activity.viewModels
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatButton
 import androidx.core.view.GravityCompat
+import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.planer.R
 import com.example.planer.databinding.ActivityMainBinding
+import com.example.planer.databinding.CustomDialogAddScheduleBinding
 import com.example.planer.model.PlanDto
 import com.example.planer.view.plan.GetPlanActivity
 import com.example.planer.view.user.LoginActivity
+import com.example.planer.viewmodel.ScheduleViewModel
 import com.example.planer.viewmodel.adapter.MainPlanListRecyclerAdapter
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import kotlin.collections.ArrayList
 import android.view.View as View1
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private val auth = FirebaseAuth.getInstance()
     private val database = FirebaseDatabase.getInstance()
+
+    private val viewModel: ScheduleViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -63,13 +74,73 @@ class MainActivity : AppCompatActivity() {
         binding.navigationDrawer.setNavigationItemSelectedListener {
             when(it.itemId) {
                 R.id.create_time_plan -> {
-                    Toast.makeText(this, "create_time_plan", Toast.LENGTH_SHORT).show()
+                    dialogLogic()
                 }
                 R.id.modify_delete_time_plan -> {
                     Toast.makeText(this, "modify_delete_time_plan", Toast.LENGTH_SHORT).show()
                 }
             }
             return@setNavigationItemSelectedListener true
+        }
+    }
+
+    private fun dialogLogic() {
+        val dialogBinding = CustomDialogAddScheduleBinding.inflate(LayoutInflater.from(this))
+        val dialogBuilder = AlertDialog.Builder(this)
+            .setView(dialogBinding.root)
+        val dialog = dialogBuilder.show()
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+        dialogBinding.setTimeModeSpinner.onItemSelectedListener = object :AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                p0: AdapterView<*>?,
+                view: android.view.View?,
+                position: Int,
+                id: Long
+            ) {
+                when(position) {
+                    0 -> {  // 시작시간
+                        dialogBinding.setScheduleTimePicker.setOnTimeChangedListener { timePicker, hour, minute ->
+                            if (hour > 12) {
+                                dialogBinding.textStartTime.text = "pm ${hour - 12}: $minute"
+                            } else {
+                                dialogBinding.textStartTime.text = "am ${hour}: $minute"
+                            }
+                        }
+                    }
+                    1 -> {  // 종료 시간
+                        dialogBinding.setScheduleTimePicker.setOnTimeChangedListener { timePicker, hour, minute ->
+                            if (hour > 12) {
+                                dialogBinding.textEndTime.text = "pm ${hour - 12}: $minute"
+                            } else {
+                                dialogBinding.textEndTime.text = "am ${hour}: $minute"
+                            }
+                        }
+                    }
+                    else -> Log.d("ERROR", "onItemSelected: 에러")
+                }
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {}
+        }
+
+        dialogBinding.addScheduleButton.setOnClickListener {
+            val startTime = dialogBinding.textStartTime.text.toString()
+            val endTime = dialogBinding.textEndTime.text.toString()
+
+            viewModel.createScheduleLogic(startTime, endTime, dialogBinding.scheduleDescriptionEditText.text.toString())
+            viewModel.result.observe(this, Observer { result ->
+                when(result) {
+                    true -> {
+                        Toast.makeText(this, "일정을 생성하였습니다.", Toast.LENGTH_SHORT).show()
+                        dialog.dismiss()
+                    }
+                    false -> {
+                        Toast.makeText(this, "일정을 생성하지 못헀습니다.", Toast.LENGTH_SHORT).show()
+                        dialog.dismiss()
+                    }
+                }
+            })
         }
     }
 
