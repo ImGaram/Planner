e0b7f5c52214c28db6e5aaa3f6662f00ad264024
imgaram <s21049@gsm.hs.kr>
2022-08-04 15:12:28 +0900

    날짜 가져오는 기능 추가

--- app/src/main/java/com/example/planer/viewmodel/ScheduleViewModel.kt
+++ app/src/main/java/com/example/planer/viewmodel/ScheduleViewModel.kt
@@ -3,11 +3,14 @@ package com.example.planer.viewmodel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.planer.model.ScheduleDto
+import com.example.planer.viewmodel.adapter.TimeTableRecyclerAdapter
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import java.text.SimpleDateFormat
+import java.util.*
 
 class ScheduleViewModel: ViewModel() {
     private val auth = FirebaseAuth.getInstance()
@@ -30,6 +33,7 @@ class ScheduleViewModel: ViewModel() {
                 scheduleDto.id = scheduleList.size + 1
                 scheduleDto.createUid = auth.currentUser?.uid
                 scheduleDto.startTime = startTime
+                scheduleDto.uploadDate = setTodayTime()
                 scheduleDto.endTime = endTime
                 scheduleDto.description = description
 
@@ -40,5 +44,13 @@ class ScheduleViewModel: ViewModel() {
 
             override fun onCancelled(error: DatabaseError) {}
         })
+    }
+
+    fun setTodayTime(): String {
+        val now = System.currentTimeMillis()
+        val date = Date(now)
+        val format = SimpleDateFormat("yyyy/MM/dd E", Locale.KOREA)
+
+        return format.format(date)
     }
 }
