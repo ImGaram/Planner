e921ca73427b8cd3136aaf03c92799b27864d479
imgaram <s21049@gsm.hs.kr>
2022-08-04 14:03:16 +0900

    생성날짜 데이터 추가

--- app/src/main/java/com/example/planer/model/ScheduleDto.kt
+++ app/src/main/java/com/example/planer/model/ScheduleDto.kt
@@ -4,6 +4,7 @@ data class ScheduleDto(
     var id: Int? = null,
     var startTime: String? = null,
     var endTime: String? = null,
+    var uploadDate: String? = null,
     var description: String? = null,
     var createUid: String? = null
 )
